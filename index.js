5f55543a9f07ae72e597aee948623f1233198e3e
Wade Coats Jr. <149808971+WCC901@users.noreply.github.com>
2023-11-22 00:09:31 -0500

    Updated the isNotOver25 variable and gave the console more info to log

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isNotOver25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -30,7 +30,7 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isValid = isSum50 && isTwoOdd && !isNotOver25 && isUnique;
 
 // Check five: all numbers divisible by 5
 // Similar logic to the isTwoOdd variable 
@@ -45,7 +45,14 @@ const fnGreaterThanln = n1 > n4;
 const arithmeticChain = ((n2 - n1) * n3) % n4
 
 // Finally, log the results.
-console.log(arithmeticChain);
+console.log(`The sum of the four numbers is 50 because the check returns: ${isSum50}.`);
+console.log(`At least 2 of the numbers are odd because the check returns: ${isTwoOdd}`);
+console.log(`None of the numbers are over 25 because the check returns: ${isNotOver25}`);
+console.log(`All 4 of the numbers are unique because the check returns: ${isUnique}`);
+console.log(`Checks that the numbers clear all prior check returns: ${isValid}`);
+console.log(`All numbers are divisible by 5 because the check returns: ${isDivBy5}`);
+console.log(`The first number (n1) is greater than the last one (n4) because the check returns: ${fnGreaterThanln}`);
+console.log(`Performs the given arithmetic and returns the answer: ${arithmeticChain}`);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
