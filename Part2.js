51ea2947a0fd4348fd1a969d586320f12fedeeff
Wade Coats Jr. <149808971+WCC901@users.noreply.github.com>
2023-11-22 02:47:56 -0500

    Added variables and console logs to solve the second half and answered which speed will make the most sense

--- Part2.js
+++ Part2.js
@@ -19,4 +19,14 @@ let is55Affordable = budget >= (costPerGallon * totalGallons55);
 let is60Affordable = budget >= (costPerGallon * totalGallons60);
 let is75Affordable = budget >= (costPerGallon * totalGallons75);
 
-console.log(`Is going 55 mph affordable?: ${is55Affordable}\nIs going 60 mph affordable?: ${is60Affordable}\nIs going 75 mph affordable?: ${is75Affordable}`);
+console.log(`Is going 55 mph affordable?: ${is55Affordable}\nIs going 60 mph affordable?: ${is60Affordable}\nIs going 75 mph affordable?: ${is75Affordable}\n`);
+
+// Calculates the time (in hours)
+// that the entire trip will TrackEvent.
+let lengthOfTrip55 = (fullDistance/55);
+let lengthOfTrip60 = (fullDistance/60);
+let lengthOfTrip75 = (fullDistance/75);
+
+console.log(`When traveling at 55 mph, the trip will be ${lengthOfTrip55} hours, at 60mph, ${lengthOfTrip60} hours, and at 75 mph, ${lengthOfTrip75} hours.\n`);
+
+console.log("Based off the info, going 60mph seems to make the most sense as it's affordable and it'll get you there in good time.")
